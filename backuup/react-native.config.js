f22b3f0bef62cc16a90462c095863a135f08253f
Sai Rohith Sivvam <129605198+RohithSais@users.noreply.github.com>
2025-10-16 23:33:23 +0530

    Add files via upload

---
+++ backuup/react-native.config.js
@@ -0,0 +1,8 @@
+// react-native.config.js
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/src/assets/fonts/'], // Points to the fonts folder you created
+};
